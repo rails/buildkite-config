24d25dd166fdfdc9f63a45999b382bb89450b108
Rafael Mendonça França <rafael@rubyonrails.org>
2024-01-03 23:48:49 +0000

    Move build queue and run queue to inside an object

--- lib/buildkite_config.rb
+++ lib/buildkite_config.rb
@@ -2,5 +2,6 @@ module Buildkite
   module Config
     autoload :Annotate, File.expand_path("buildkite_config/annotate", __dir__)
     autoload :Diff, File.expand_path("buildkite_config/diff", __dir__)
+    autoload :Generate, File.expand_path("buildkite_config/generate", __dir__)
   end
 end
