919c9fac6344827931060d16483a86863af9161e
zzak <zzakscott@gmail.com>
2024-03-11 16:18:48 +0900

    Introduce RakeCommand::Step
    This module extends the buildkite-builder Steps::Command to provide additional functions, such as `to_label` and `build_env` without having to create local variables (which don't clash with existing DSL methods).

--- lib/buildkite/config/rake_command.rb
+++ lib/buildkite/config/rake_command.rb
@@ -4,30 +4,19 @@ require "buildkite-builder"
 
 module Buildkite::Config
   class RakeCommand < Buildkite::Builder::Extension
-    dsl do
+    module Step
       def to_label(ruby, dir, task)
-        str = +"#{dir} #{task.sub(/[:_]test|test:/, "")}"
+        str = +"#{dir} #{task.split(/\s/).last.sub(/[:_]test|test:/, "")}"
         str.sub!(/ test/, "")
         return str unless ruby.version
 
         str << " (#{ruby.short_ruby})"
       end
 
-      def rake(dir = "", task = "test", service: "default", pre_steps: [], &block)
-        build_context = context.extensions.find(BuildContext)
-
-        _label = to_label(build_context.ruby, dir, task)
-
-        ## Setup ENV
+      def build_env(build_context, pre_steps)
         _env = {
           IMAGE_NAME: build_context.image_base + ":" + build_context.ruby.image_name_for(build_context.build_id)
         }
-
-        if task.start_with?("mysql2:") || (build_context.rails_version >= Gem::Version.new("7.1.0.alpha") && task.start_with?("trilogy:"))
-          task = "db:mysql:rebuild #{task}"
-        elsif task.start_with?("postgresql:")
-          task = "db:postgresql:rebuild #{task}"
-        end
 
         if build_context.ruby.yjit_enabled?
           _env[:RUBY_YJIT_ENABLE] = "1"
@@ -37,8 +26,26 @@ module Buildkite::Config
           _env[:PRE_STEPS] = pre_steps.join(" && ")
         end
 
+        _env
+      end
+    end
+
+    def prepare
+      ::Buildkite::Pipelines::Steps::Command.prepend(RakeCommand::Step)
+    end
+
+    dsl do
+      def rake(dir = "", task = "test", service: "default", pre_steps: [], &block)
+        build_context = context.extensions.find(BuildContext)
+
+        if task.start_with?("mysql2:") || (build_context.rails_version >= Gem::Version.new("7.1.0.alpha") && task.start_with?("trilogy:"))
+          task = "db:mysql:rebuild #{task}"
+        elsif task.start_with?("postgresql:")
+          task = "db:postgresql:rebuild #{task}"
+        end
+
         command do
-          label _label
+          label to_label(build_context.ruby, dir, task)
           depends_on "docker-image-#{build_context.ruby.image_key}"
           command "rake #{task}"
 
@@ -65,7 +72,7 @@ module Buildkite::Config
             "shell" => ["runner", dir],
           }
 
-          env _env
+          env build_env(build_context, pre_steps)
           agents queue: build_context.run_queue
           artifact_paths build_context.artifact_paths
           automatic_retry_on(**build_context.automatic_retry_on)
