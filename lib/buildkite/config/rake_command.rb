062e4cb3a5b545b2f143b3953659b5e3a7b6c7c4
zzak <zzakscott@gmail.com>
2024-09-04 07:26:48 +0900

    Allow overriding compose env for local builds
    Now we can write this very simple pipeline for debugging builds locally.
    
    ```ruby
    Buildkite::Builder.pipeline do
      require "buildkite_config"
      use Buildkite::Config::BuildContext
      use Buildkite::Config::DockerBuild
      use Buildkite::Config::RakeCommand
      use Buildkite::Config::RubyGroup
    
      plugin :docker_compose, "docker-compose#v4.16.0"
      plugin :artifacts, "artifacts#v1.9.3"
    
      build_context.setup_rubies %w(3.3)
    
      group do
        label "build"
        build_context.rubies.each do |ruby|
          builder ruby, compose: {
            "cli_version": "2",
            "image-name": "buildkite_base",
            "cache-from": ["buildkite_base"],
            "push": "",
            "image-repository": "",
          }
        end
      end
    
      build_context.rubies.each do |ruby|
        ruby_group ruby do
          rake "actioncable", task: "test:integration", retry_on: { exit_status: -1, limit: 3 }, compose: {
            "cli_version": "2",
            "pull": "",
          }, env: {
            "IMAGE_NAME": "buildkite_base",
          }
        end
      end
    end
    ```

--- lib/buildkite/config/rake_command.rb
+++ lib/buildkite/config/rake_command.rb
@@ -20,7 +20,7 @@ module Buildkite::Config
         env ||= {}
         pre_steps ||= []
 
-        env[:IMAGE_NAME] = build_context.image_name_for(build_context.build_id, prefix: nil)
+        env[:IMAGE_NAME] ||= build_context.image_name_for(build_context.build_id, prefix: nil)
 
         if build_context.ruby.yjit_enabled?
           env[:RUBY_YJIT_ENABLE] = "1"
@@ -33,7 +33,16 @@ module Buildkite::Config
         env
       end
 
-      def install_plugins(service = "default", env = nil, dir = ".")
+      def install_plugins(service = "default", env = nil, dir = ".", compose: nil)
+        compose_options = {
+          "env" => env,
+          "run" => service,
+          "pull" => service,
+          "config" => ".buildkite/docker-compose.yml",
+          "shell" => ["runner", *dir],
+        }
+        compose_options.merge!(compose) if compose
+
         plugin :artifacts, {
             download: ".dockerignore"
           }
@@ -49,13 +58,7 @@ module Buildkite::Config
           compressed: ".buildkite.tgz"
         }
 
-        plugin :docker_compose, {
-          "env" => env,
-          "run" => service,
-          "pull" => service,
-          "config" => ".buildkite/docker-compose.yml",
-          "shell" => ["runner", *dir],
-        }.compact
+        plugin :docker_compose, compose_options.compact
       end
     end
 
@@ -64,7 +67,7 @@ module Buildkite::Config
     end
 
     dsl do
-      def bundle(command, label:, env: nil)
+      def bundle(command, label:, env: nil, compose: nil)
         build_context = context.extensions.find(BuildContext)
 
         command do
@@ -72,7 +75,7 @@ module Buildkite::Config
           depends_on "docker-image-#{build_context.ruby.image_key}"
           command command
 
-          install_plugins
+          install_plugins(compose: compose)
 
           env build_env(build_context, nil, env)
 
@@ -84,7 +87,7 @@ module Buildkite::Config
         end
       end
 
-      def rake(dir, task: "test", label: nil, service: "default", pre_steps: nil, env: nil, retry_on: nil, soft_fail: nil, parallelism: nil)
+      def rake(dir, task: "test", label: nil, service: "default", pre_steps: nil, env: nil, retry_on: nil, soft_fail: nil, parallelism: nil, compose: nil)
         build_context = context.extensions.find(BuildContext)
 
         if task.start_with?("mysql2:") || (build_context.rails_version >= Gem::Version.new("7.1.0.alpha") && task.start_with?("trilogy:"))
@@ -98,7 +101,7 @@ module Buildkite::Config
           depends_on "docker-image-#{build_context.ruby.image_key}"
           command "rake #{task}"
 
-          install_plugins(service,  %w[PRE_STEPS RACK], dir)
+          install_plugins(service,  %w[PRE_STEPS RACK], dir, compose: compose)
 
           env build_env(build_context, pre_steps, env)
 
